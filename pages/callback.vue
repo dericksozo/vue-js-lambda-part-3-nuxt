59ff8a0519e97acaf62b06a7a823d5c7da88976a
Derick Ruiz <derick.r.ruiz@gmail.com>
2018-09-27 19:39:02 +0700

    Adding the callback route from client/src/components/Callback into pages/callback.vue

---
+++ pages/callback.vue
@@ -0,0 +1,26 @@
+<script>
+import * as Auth0 from 'auth0-web'
+
+export default {
+  name: 'Callback',
+  created () {
+    Auth0.handleAuthCallback(() => {
+      this.$router.push('/')
+    })
+  }
+}
+</script>
+
+<template>
+<div>
+  <p>Loading your profile...</p>
+</div>
+</template>
+
+<style scoped>
+div > p {
+  margin: 0 auto;
+  text-align: center;
+  font-size: 22px;
+}
+</style>
