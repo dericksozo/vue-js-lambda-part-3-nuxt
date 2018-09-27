02e15705ed4c115cd82b3381c959bf2f57bf54d9
Derick Ruiz <derick.r.ruiz@gmail.com>
2018-09-27 19:31:41 +0700

    Adding client/src/App.vue as the default layout.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,35 @@
 <template>
-  <div>
-    <nuxt/>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
+<script>
+import * as Auth0 from 'auth0-web'
+
+export default {
+  name: 'App',
+  created () {
+    Auth0.configure({
+      domain: 'alien-go-home.auth0.com',
+      clientID: 'NQipGcpZsGn0CEpJPbVFoKZDY0I7WuVP',
+      audience: 'https://micro-blog-app',
+      redirectUri: 'http://localhost:8080/callback',
+      responseType: 'token id_token',
+      scope: 'openid profile'
+    })
+  }
+}
+</script>
+
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
