845a4fd5e8428a2007cd76a251630bf39d6dd605
Derick Ruiz <derick.r.ruiz@gmail.com>
2018-09-28 15:27:33 +0700

    Adding the MicroPostsService from client/src/MicroPostsService.js into its own services folder.

---
+++ services/MicroPostsService.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import * as Auth0 from 'auth0-web'
+
+const url = 'http://localhost:8080/micro-posts/'
+
+class MicroPostsService {
+  static getMicroPosts () {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const serverResponse = await axios.get(url)
+        const unparsedData = serverResponse.data
+        resolve(unparsedData.map(microPost => ({
+          ...microPost,
+          createdAt: new Date(microPost.createdAt)
+        })))
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  static insertMicroPost (text) {
+    console.log("Auth0 MicroPostsService");
+    console.log("text", text);
+    return axios.post(url, {
+      text
+    }, {
+      headers: { 'Authorization': `Bearer ${Auth0.getAccessToken()}` }
+    })
+  }
+}
+
+export default MicroPostsService
