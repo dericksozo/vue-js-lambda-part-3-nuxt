c498522fcd5d04e107ad5b728c085dd62cac84ec
Derick Ruiz <derick.r.ruiz@gmail.com>
2018-09-28 15:27:59 +0700

    Adding the routes from backend/src/routes.js into the server folder.

---
+++ server/routes.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const auth0 = require('auth0');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+const router = express.Router();
+
+// retrieve latest micro-posts
+router.get('/', async (req, res) => {
+  const collection = await loadMicroPostsCollection();
+  res.send(
+    await collection.find({}).toArray()
+  );
+});
+
+// this is a middleware to validate access_tokens
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://alien-go-home.auth0.com/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: 'https://micro-blog-app',
+  issuer: `https://alien-go-home.auth0.com/`,
+  algorithms: ['RS256']
+});
+
+// insert a new micro-post with user details
+router.post('/', checkJwt, async (req, res) => {
+  const collection = await loadMicroPostsCollection();
+
+  const token = req.headers.authorization
+    .replace('bearer ', '')
+    .replace('Bearer ', '');
+
+  const authClient = new auth0.AuthenticationClient({
+    domain: 'alien-go-home.auth0.com',
+    clientId: 'NQipGcpZsGn0CEpJPbVFoKZDY0I7WuVP',
+  });
+
+  authClient.getProfile(token, async (err, userInfo) => {
+    if (err) {
+      console.log("It's erroring out here.");
+      console.log("Err", err);
+      return res.status(500).send(err);
+    }
+
+    await collection.insertOne({
+      text: req.body.text,
+      createdAt: new Date(),
+      author: {
+        sub: userInfo.sub,
+        name: userInfo.name,
+        picture: userInfo.picture,
+      },
+    });
+
+    res.status(200).send();
+  });
+});
+
+async function loadMicroPostsCollection() {
+  const client = await MongoClient.connect('mongodb://localhost:27017/');
+  return client.db('micro-blog').collection('micro-posts');
+}
+
+module.exports = router;
