3c57108a2885c06375b6cdea53dacd78009e896f
Derick Ruiz <derick.r.ruiz@gmail.com>
2018-09-30 01:57:37 +0900

    Removing refernence to bodyParser and routes because it's now in /api. Making sure this is purely for rendering nuxt universally.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 
 const express = require('express')
 const consola = require('consola')
@@ -5,9 +6,6 @@ const { Nuxt, Builder } = require('nuxt')
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
-const bodyParser = require('body-parser');
-
-const routes = require('./routes');
 
 app.set('port', port)
 
@@ -24,12 +22,6 @@ async function start() {
     const builder = new Builder(nuxt)
     await builder.build()
   }
-
-  // use bodyParser to parse application/json content-type
-  app.use(bodyParser.json());
-  
-  // The API
-  app.use('/micro-posts', routes);
 
   // Give nuxt middleware to express
   app.use(nuxt.render)
