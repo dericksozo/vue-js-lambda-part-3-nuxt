d6d9fc028ba8db30a022c5f7a5064010fd79b8a7
Derick Ruiz <derick.r.ruiz@gmail.com>
2018-09-30 02:01:41 +0900

    Adding method to grab an individual post, and also adding ability to post body.

--- services/MicroPostsService.js
+++ services/MicroPostsService.js
@@ -1,9 +1,26 @@
 import axios from 'axios'
 import * as Auth0 from 'auth0-web'
 
-const url = 'http://localhost:8080/micro-posts/'
+const url = 'http://localhost:8081/micro-posts/'
 
 class MicroPostsService {
+
+  /*
+   * @id:Number - Optional parameter to get a specific post.
+   */
+  static getMicroPost(id) {
+    return new Promise( async (resolve, reject) => {
+      try {
+        const serverResponse = await axios.get(url + id);
+        let microPost = serverResponse.data;
+        microPost.createdAt = new Date(microPost.createdAt);
+        resolve(microPost);
+      } catch (error) {
+        reject(error);
+      }
+    })
+  }
+
   static getMicroPosts () {
     return new Promise(async (resolve, reject) => {
       try {
@@ -19,11 +36,10 @@ class MicroPostsService {
     })
   }
 
-  static insertMicroPost (text) {
-    console.log("Auth0 MicroPostsService");
-    console.log("text", text);
+  static insertMicroPost (text, body) {
     return axios.post(url, {
-      text
+      text,
+      body
     }, {
       headers: { 'Authorization': `Bearer ${Auth0.getAccessToken()}` }
     })
