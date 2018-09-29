a1d4061dd5a38a5d7c257292d2fce1adf7622c4f
Derick Ruiz <derick.r.ruiz@gmail.com>
2018-09-29 18:22:17 +0900

    Adding the entire API that takes care of getting and posting POSTS from the Mongo Store into its own folder, to later upload it separately into AWS Lambda.

---
+++ api/dev-server.js
@@ -0,0 +1,6 @@
+let app = require("./index.js");
+
+// start the server
+app.listen(8081, () => {
+  console.log('listening on port 8081');
+});
---
+++ api/index.js
@@ -0,0 +1,26 @@
+//import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const routes = require('./routes');
+
+// define the Express app
+const app = express();
+
+// enhance your app security with Helmet
+app.use(helmet());
+
+// use bodyParser to parse application/json content-type
+app.use(bodyParser.json());
+
+// enable all CORS requests
+app.use(cors());
+
+// log HTTP requests
+app.use(morgan('combined'));
+
+app.use('/micro-posts', routes);
+
+module.exports = app;
